dbdc7690a2ab68caf5d94dbd25f0af6bc9ea835e
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2024-01-06 20:47:56 -0500

    modificacion de modelos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,7 +36,7 @@ $routes->post("guardarSemestre" , "Semestres::guardar_semestre");
 $routes->get('previos', 'Previos::listar_previos');
 $routes->get('insertar-previos', 'Previos::insertar_previos');
 $routes->post('guardar-previos', 'Previos::guardar_previos');
-$routes->get('editar-previo/(:num)', 'Previos::editar_previos/$1');
+$routes->get('editar-previo/(:string)', 'Previos::editar_previos/$1');
 
 $routes->get('borrar-previo/(:string)', 'Previos::borrar_previo/$1');
 
--- app/Models/Previo.php
+++ app/Models/Previo.php
@@ -7,5 +7,9 @@ class Previo extends Model{
     protected $table      = 'previos';
     // Uncomment below if you want add primary key
     protected $primaryKey = 'tipo_previo';
+    protected $useAutoIncrement = true;
+    
+    protected $returnType     = 'array';
+
     protected $allowFields = ['tipo_previo','porcentaje'];
 }
